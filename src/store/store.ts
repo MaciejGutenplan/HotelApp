818b5d4d0c58713a5ca084aa6b547d43ba767a94
Maciej.Gutenplan <Maciej.Gutenplan@itechart-group.com>
2022-09-28 17:12:34 +0200

    [Add] Save redux state to local storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,19 +4,40 @@ import { reducer as HotelReducer } from './hotel/reducer'
 import { reducer as CountryReducer } from "./country/reducer";
 import { reducer as CityReducer} from "./city/reducer";
 
-const InitialState = {
+const DefaultState = {
     countries: [{ id: 0, name: 'Test country' }],
     cities: [{ id: 0, name: 'Test city', countryId: 0}],
     hotels: [{ id: 0, name: 'Test hotel', price: 666, address: 'Test address', countryId: 0, cityId: 0 }]
+}
+
+const saveToLocalStorage = (state: RootState) => {
+    try {
+        const serialisedState = JSON.stringify(state);
+        localStorage.setItem("persistentLocalData", serialisedState);
+    } catch (e) {
+        console.warn(e);
+    }
+}
+
+const loadFromLocalStorage = () => {
+    try {
+        const storage = localStorage.getItem("persistentLocalData");
+        if (storage === null) return DefaultState;
+        return JSON.parse(storage);
+    } catch (e) {
+        console.warn(e);
+    }
 }
 
 const store = createStore(combineReducers({
     hotels: HotelReducer,
     countries: CountryReducer,
     cities: CityReducer
-}), InitialState, composeWithDevTools(
+}), loadFromLocalStorage(), composeWithDevTools(
     applyMiddleware()
 ));
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
 
